fb0629eb87f5af0211c9eca5a250b6ae8fcd5c1b
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2023-08-25 15:41:38 +0300

    1

---
+++ project/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System;
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        Console.Write("Dosya ismi girin:");
+        string directorName = Console.ReadLine();
+        string pathOfDirectory = "/Users/kadirdundar/Desktop/projeKuveyt/project/" + directorName + "/";
+
+        string[] directoryNames = {"AnaKaynaklar","Arşiv","CS_audit","Denetim","Education_Consultanty_Materials"};
+
+        try
+        {
+            foreach (var directory in directoryNames)
+            {
+               Directory.CreateDirectory(pathOfDirectory + directory);
+            }
+        }
+        catch (System.Exception ex)
+        {
+            
+            Console.WriteLine("dosya oluşturulurken bir hata alındı" + ex.Message);
+        }
+    }
+    }
+}
