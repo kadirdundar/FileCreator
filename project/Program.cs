dd4552a7658d636455f467411f8b7ba0d840c51f
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2023-08-28 15:13:42 +0300

    Update Program.cs

--- project/Program.cs
+++ project/Program.cs
@@ -1,36 +1,93 @@
-﻿using System.IO;
 using System;
-namespace Project
+using System.IO;
+using System.Security.AccessControl;
+
+class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
+    static void Main(string[] args)
     {
         Console.Write("Dosya ismi girin:");
-        string directorName = Console.ReadLine();
+        string directorName = Console.ReadLine();// istenilen dosya ismi
         string pathOfDirectory = "/Users/kadirdundar/Desktop/projeKuveyt/project/" + directorName + "/";
+        Console.WriteLine("Oluşturmak istediğiniz dosyalama türü; 1:Denetim, 2:Raporlar, 3:Tutanaklar")
+        int choice = Console.ReadLine()
 
-        string[] directoryNames = {"AnaKaynaklar","Arşiv","CS_audit","Denetim","Education_Consultanty_Materials"};
+        //Dosyaların içereceği klasörler(bir tanesi seçilecektir)
+        string[] directoryNames1 = {"AnaKaynaklar","Arşiv","CS_audit","Denetim","Education_Consultanty_Materials"};
+        string[] directoryNames2 = {"Rapor1","Rapor2","Rapor3"};
+        string[] directoryNames3 = {"Tutanak1","Tutanak2","Tutanak3"};
 
-        string[] allowedUsers = {"kadir", "Aüstad","Büstad"};
-
-        string username = Environment.UserName;
-
-        if (Array.Exists(allowedUsers, element => element == username)){
-
+        string[] selectedDirectoryNames;
+        switch (choice)
+        {
+            case 1:  selectedDirectoryNames = directoryNames1 break;
+            case 2: selectedDirectoryNames = directoryNames2 break;
+            case 3: selectedDirectoryNames = directoryNames3 break;
+            
+            default:
+                break;
+        }
+        createFolder(selectedDirectoryNames,pathOfDirectory);
+        provideAccsess();
+    }
+    static void provideAccsess(){//Klasörlere erişim izini verilmesi 
         try
         {
-            foreach (var directory in directoryNames)
+            // İzinleri temsil eden dizi
+            string[][] permissions = new string[][]
             {
-               Directory.CreateDirectory(pathOfDirectory + directory);
+                new string[] { "kadir", "FullControl", "Allow" },
+                new string[] { "admins", "Read", "Allow" },
+                new string[] { "guests", "Read", "Deny" }
+            };
+
+            // Dosyanın izinlerini ayarlamak için FileSecurity sınıfından bir obje oluşturduk 
+            FileSecurity fileSecurity = File.GetAccessControl(fileName);
+
+            // İzinleri diziye ekledik
+            foreach (string[] permission in permissions)
+            {
+                string userOrGroup = permission[0];
+                FileSystemRights rights = (FileSystemRights)Enum.Parse(typeof(FileSystemRights), permission[1]);//Enum.parse ile type'ı değiştirdik
+                AccessControlType controlType = (AccessControlType)Enum.Parse(typeof(AccessControlType), permission[2]);
+
+                fileSecurity.AddAccessRule(new FileSystemAccessRule(userOrGroup, rights, controlType));
+            }
+
+            // Dosyanın izinlerini güncelledik
+            File.SetAccessControl(directorName, fileSecurity);
+
+            Console.WriteLine("Dosya izinleri başarıyla ayarlandı.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Dosya izinleri ayarlanırken bir hata oluştu: " + ex.Message);
+        }
+
+    }
+    static void createFolder(string[] selelectedType,string pathOfDirectoryy){
+           try
+        {
+            foreach (var directory in selelectedType)
+            {
+               Directory.CreateDirectory(pathOfDirectoryy + directory);//dosyaların oluşturulması
             }
         }
         catch (System.Exception ex)
         {
-            
             Console.WriteLine("dosya oluşturulurken bir hata alındı" + ex.Message);
         }
+ 
+
     }
-    }
+
+    static string[] getInformationAuthority(){//yetkili kullanıcıların bilgisini çekmek için
+
+
+        string klasorYolu = @"\\sunucu\paylasilanKlasor";
+        string[] dosyaIsimleri = Directory.GetFiles(klasorYolu);
+
+       return dosyaIsimleri;
+    
     }
 }
